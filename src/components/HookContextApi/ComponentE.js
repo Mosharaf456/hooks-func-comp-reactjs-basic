119647ab6704e55bf2708427d7d35a584be95672
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-18 16:45:57 +0600

    ref useContext hook added

--- src/components/HookContextApi/ComponentE.js
+++ src/components/HookContextApi/ComponentE.js
@@ -1,28 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext, ChannelContext } from '../../App'
+
 /*
 Context provides a way to pass data through the component tree without having to pass props down manually at every level.
 */
-import { UserContext, ChannelContext } from '../../App'
-// useContext hook is used to consume the context value in a better way.
 
 function ComponentE() {
+  const user = useContext(UserContext)
+  const channel = useContext(ChannelContext)
+
   return (
     <>
-      <UserContext.Consumer>
-        {
-          user => {
-            return (
-              <ChannelContext.Consumer>
-                {
-                  channel => {
-                    return <div>User context value {user}, channel context value {channel}</div>
-                  }
-                }
-              </ChannelContext.Consumer>
-            )
-          }
-        }
-      </UserContext.Consumer>
+      <div>Component E</div>
+      <div>User context value {user} -  channel context value {channel}</div>
     </>
   )
 }
