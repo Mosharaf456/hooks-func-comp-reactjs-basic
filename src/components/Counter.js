20c3aacc5078d02a1243d1939584a1332c6723fe
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-18 10:52:15 +0600

    ref hooks , useState hooks basic code added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+// hooks are just function we simply call them into our functional component
+
+/* Basic part of Hooks:
+useState is a Hook function that lets you add React state to the functional components.
+Steps of useState Hook: 
+1. A created component will use. 
+2. state property initialized with 0 default value but default value is never used to re-render the component.
+3. A method to update the state property and that will cost the component to re-render.
+*/
 
 function Counter() {
-    // state property initialized with 0
-    // useState is a Hook that lets you add React state to function components.
+  /* useState accept the initial value of the state property 
+    and returns the current value of the state property and a method to update the state property.
+    
+    //It is array destructuring const [count , setCount] which is feature of ES6.
+  */
+  const [count , setCount] = useState(0);
+  
   return (
     <div>
-        <button onclick={}>+</button>
-        <h1>Counter {}</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <h1>Counter {count}</h1>
         <button>-</button>
     </div>
   )
 }
+
+/*
+Note:
+Only call Hooks at the top level. **
+Don’t call Hooks inside loops, conditions, or nested functions.**
+
+**Make sure :
+Only call Hooks from React function components.**
+Don’t call Hooks from regular JavaScript functions.**
+*/
 
 export default Counter
