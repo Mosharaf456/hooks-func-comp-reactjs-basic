2bd7a57055ed74ae8422d4f3db1bb4ea5547cd13
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-21 14:06:28 +0600

    ref useReducer Vs useState difference commented or documented

--- src/components/HookUseReducer/DataFetching/DatafetchingTwo.js
+++ src/components/HookUseReducer/DataFetching/DatafetchingTwo.js
@@ -1,6 +1,31 @@
 import React, { useEffect, useReducer } from 'react'
 import axios from 'axios';
 
+/*
+useState Vs useReducer
+1. When to use useState
+- Type of state - Number, String, Boolean
+- Number of state transitions - One or Two
+- Related state transitions - Unrelated state transitions
+- Business logic - No business logic
+- Local vs Global - Local
+
+    - When dealing with simple state or single state value.
+    - When state transition does not depend on the previous state.
+    - When the logic is simple and does not involve multiple sub-values.
+2. When to use useReducer
+- Type of state - Object or Array
+- Number of state transitions - Too many
+- Related state transitions - Related state transitions
+- Business logic - Complex business logic
+- Local vs Global - Global state better managed with useReducer
+
+    - When state logic is complex and involves multiple sub-values.
+    - When next state depends on the previous state.
+    - When you have similar states that can be grouped together.
+    - When the logic is complex and involves multiple sub-values.
+    - When you have multiple actions that leads to state transitions.
+*/
 const initialState = {
     loading: true,
     error: '',
